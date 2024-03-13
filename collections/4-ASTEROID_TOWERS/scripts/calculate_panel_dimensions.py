f64042cc8b4fb0b8b58143c98f924f7ba39f5020
flarnrules <breifenberg11@gmail.com>
2024-03-13 02:23:28 -0500

    thru pt 350?

---
+++ collections/4-ASTEROID_TOWERS/scripts/calculate_panel_dimensions.py
@@ -0,0 +1,13 @@
+
+
+new_canvas_grid_x = 5
+current_canvas_x = 768
+
+new_canvas_grid_y = new_canvas_grid_x
+current_canvas_y = current_canvas_x
+
+panel_x = current_canvas_x / new_canvas_grid_x
+panel_y = panel_x
+panel_size = f"{panel_y}x{panel_x}"
+
+print(f"The panel size you want is: {panel_size}")
